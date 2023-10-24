7cee9cf2c86302da6fb9301ef2155a4cb6054d08
MohnishPurella <purella1597@gmail.com>
2023-10-24 13:10:54 +0530

    Interpolation

--- binding-demo/src/app/app.component.ts
+++ binding-demo/src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'binding-demo';
+export class AppComponent {  
+  showInterpolation:boolean = false;
+
+  showInterpolationFun(){
+    this.showInterpolation = true;
+  }
 }
