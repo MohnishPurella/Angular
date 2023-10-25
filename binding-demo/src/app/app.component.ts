22f9bd848bbcff9aa612e244da5185b226a42bf8
MohnishPurella <purella1597@gmail.com>
2023-10-25 22:10:42 +0530

    Style Binding

--- binding-demo/src/app/app.component.ts
+++ binding-demo/src/app/app.component.ts
@@ -9,21 +9,32 @@ export class AppComponent {
   showInterpolation:boolean = false;
   showPropertyBinding: boolean = false;
   showClassBinding:boolean = false;
+  showStyleBinding:boolean = false;
 
   showInterpolationFun(){
     this.showInterpolation = true;
     this.showPropertyBinding = false;
     this.showClassBinding = false;
+    this.showStyleBinding = false;
   }
 
   showPropertyBindingFun(){
     this.showPropertyBinding = true;
     this.showInterpolation = false;
     this.showClassBinding = false;
+    this.showStyleBinding = false;
   }
 
   showClassBindingFun(){
     this.showClassBinding = true;
+    this.showInterpolation = false;
+    this.showPropertyBinding = false;
+    this.showStyleBinding = false;
+  }
+
+  showStyleBindingFun(){
+    this.showStyleBinding = true;
+    this.showClassBinding = false;
     this.showInterpolation = false;
     this.showPropertyBinding = false;
   }
--- binding-demo/src/app/app.module.ts
+++ binding-demo/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { InterpolationComponent } from './components/interpolation/interpolation.component';
 import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
 import { ClassBindingComponent } from './components/class-binding/class-binding.component';
+import { StyleBindingComponent } from './components/style-binding/style-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InterpolationComponent,
     PropertyBindingComponent,
-    ClassBindingComponent
+    ClassBindingComponent,
+    StyleBindingComponent,
   ],
   imports: [
     BrowserModule
